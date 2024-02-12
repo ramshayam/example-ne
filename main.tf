93b733b99d3e943b667fef35f73b91f22034e3f7
test <test@yser>
2024-02-12 13:05:49 +0530

    new1

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ resource "github_repository" "example" {
  
 }
 
-/*resource "aws_instance" "name" {
+resource "aws_instance" "name" {
   ami = "ami-0d63de463e6604d0a"
   availability_zone = var.avi_zone
   instance_type = var.instance_type
   tags = {
     Name = "FirstInsatnce"
   }
-}*/
+}
